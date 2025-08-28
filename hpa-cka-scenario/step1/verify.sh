154ede5d7cd3e359d48f709530ef89ffb7b95956
Sylvain Reynaud <github.egotistic301@passmail.net>
2025-08-28 14:09:06 +0200

    fix: update HPA verification to use metrics[0].resource.target.averageUtilization path

--- hpa-cka-scenario/step1/verify.sh
+++ hpa-cka-scenario/step1/verify.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-kubectl get hpa apache-server -n autoscale -o=jsonpath='{.spec.targetCPUUtilizationPercentage}{.spec.minReplicas}{.spec.maxReplicas}{.spec.behavior.scaleDown.stabilizationWindowSeconds}' | grep -q '501430'
+kubectl get hpa apache-server -n autoscale -o=jsonpath='{.spec.metrics[0].resource.target.averageUtilization}{.spec.minReplicas}{.spec.maxReplicas}{.spec.behavior.scaleDown.stabilizationWindowSeconds}' | grep -q '501430'
